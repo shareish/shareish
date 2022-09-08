cec9477ac9614eaf4f82c9a8be5d47814a25e4c7
rmaree <raphael.maree@ulg.ac.be>
2022-09-08 17:22:29 +0200

    improve map tiles and colorized markers

--- frontend/src/views/dashboard/DashboardView.vue
+++ frontend/src/views/dashboard/DashboardView.vue
@@ -90,7 +90,6 @@
                 </div>
             </div>                  
         </div>
-        <p>If you encounter problems about the tiles loading, please report this problem to <a href="https://www.openstreetmap.org/fixthemap">Open Street Map</a>.</p>
     </div>
 </template>
 
@@ -108,10 +107,12 @@ export default {
             show: false,
             filter: {},
             items: [],
+	    item_type : {},
             markers: {},
             modalToShow: {},
             nameToShow: '',
             descriptionToShow: '',
+	    typeToShow: '',
             latitude: null,
             longitude: null,
             zoom: null,
@@ -134,10 +135,10 @@ export default {
             this.longitude = position.coords.longitude
             this.zoom = 16
             this.map = L.map('mapid').setView([this.latitude, this.longitude], this.zoom)
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png ', {
+            L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png ', {
                 maxZoom: 19,
                 zoom: 16,
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
             }).addTo(this.map);
 
             L.easyButton('<i class="fa fa-map-marker"><i>', function(btn, map){
@@ -145,7 +146,10 @@ export default {
             }).addTo(this.map);
 
             this.filter.item_type = 'null'
-            this.markers = new L.markerClusterGroup()
+            this.markers = new L.markerClusterGroup({
+		disableClusteringAtZoom: 16,
+		chunkedLoading: true,
+	    })
             this.getItemsLocation()
         }
 
@@ -154,14 +158,15 @@ export default {
             this.longitude = 5.560470
             this.zoom = 14
             this.map = L.map('mapid').setView([this.latitude, this.longitude], this.zoom)
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png ', {
+            L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png ', {
                 maxZoom: 19,
                 zoom: 16,
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
             }).addTo(this.map);
 
             this.filter.item_type = 'null'
-            this.markers = new L.markerClusterGroup()
+	    this.markers = new L.markerClusterGroup({
+		disableClusteringAtZoom: 16})
             this.getItemsLocation()
         }
 
@@ -178,6 +183,8 @@ export default {
                     this.items = response.data
                     for(let i = 0; i < response.data.length; i++){
                         if(response.data[i]['location'] != null){
+			    //console.log(response.data[i])
+			    //console.log("---")
                             let refinedLocation = response.data[i]['location'].slice(17, -1)
                             const arrlocations = refinedLocation.split(" ")
                             let location = {
@@ -195,6 +202,7 @@ export default {
                     console.log(error)
                 })    
         },
+	
         async onMarkerClick(e){
             this.modalToShow = this.items[e.target.increment]
             await axios
@@ -202,6 +210,7 @@ export default {
                 .then(response => {
                     this.nameToShow = response.data['name']
                     this.descriptionToShow = response.data['description']
+		    this.typeToShow = response.data['item_type']
                 })
                 .catch(error => {
                     console.log(error)
@@ -209,9 +218,10 @@ export default {
             let elem = document.getElementById("modal")
             elem.classList.add("is-active")
         },
+	
         async onMarkerHover(e){
             this.popup.setLatLng(e.latlng)
-            this.popup.setContent("Coucou ça charge")
+            //this.popup.setContent("Coucou ça charge")
             this.popup.openOn(this.map)
             if(this.item_images.has(e.target.item_id) == false){
                 const formData = new FormData()
@@ -230,31 +240,111 @@ export default {
                 this.popup.setContent('<figure class="image"><img src="' + this.item_images.get(e.target.item_id) + '" alt="Placeholder image" style="object-fit: cover; width: 100%; height: 100%"></figure>')
             }
         },
-        getItemImage(id){
-            return this.item_images.get(id)
-        },
+	
+	async getItemType(id){
+	    await axios
+		    .get(`/api/v1/items/${id}`)
+                    .then(response => {
+			this.item_type = response.data['item_type']
+                    })
+                .catch(error => {
+                    console.log(error)
+                })
+	},
+
         onMarkerOut(e){
             this.popup.close()            
         },
-        closeEdit(){
+
+	closeEdit(){
             let elem = document.getElementById("modal")
             elem.classList.remove("is-active")
             this.modalToShow = {}
         },
-        addMarkersLocation(){
+
+	async addMarkersLocation(){
             if(this.map.hasLayer(this.markers)){
                 this.markers.clearLayers()
             }
+
+	    var progress = document.getElementById('progress');
+	    var progressBar = document.getElementById('progress-bar');
+
+	    function updateProgressBar(processed, total, elapsed, layersArray) {
+		if (elapsed > 1000) {
+		    // if it takes more than a second to load, display the progress bar:
+		    progress.style.display = 'block';
+		    progressBar.style.width = Math.round(processed/total*100) + '%';
+		}
+
+		if (processed === total) {
+		    // all markers processed - hide the progress bar:
+		    progress.style.display = 'none';
+		}
+	    }
+	    
+	    //colored icons for markers
+	    var greenIcon = new L.Icon({
+		iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
+		shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+		iconSize: [25, 41],
+		iconAnchor: [12, 41],
+		popupAnchor: [1, -34],
+		shadowSize: [41, 41]
+	    });
+	    var yellowIcon = new L.Icon({
+		iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-yellow.png',
+		shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+		iconSize: [25, 41],
+		iconAnchor: [12, 41],
+		popupAnchor: [1, -34],
+		shadowSize: [41, 41]
+	    });
+	    var redIcon = new L.Icon({
+		iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+		shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+		iconSize: [25, 41],
+		iconAnchor: [12, 41],
+		popupAnchor: [1, -34],
+		shadowSize: [41, 41]
+	    });
+	    var greyIcon = new L.Icon({
+		iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-grey.png',
+		shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+		iconSize: [25, 41],
+		iconAnchor: [12, 41],
+		popupAnchor: [1, -34],
+		shadowSize: [41, 41]
+	    });
+
+	    var markerList = []
             for(let i = 0; i < this.locations.length; i++){
-                let marker = L.circleMarker([this.locations[i]['location_y'], this.locations[i]['location_x']])
+		//marker colorization based on item_type
+		var marker = {}
+		await this.getItemType(this.locations[i]['id'])
+		if (this.item_type==="DN"){
+		    marker = L.marker([this.locations[i]['location_y'], this.locations[i]['location_x']], {icon: greenIcon})
+		}
+		else if (this.item_type==="LN"){
+		    marker = L.marker([this.locations[i]['location_y'], this.locations[i]['location_x']], {icon: yellowIcon})
+		}
+		else if (this.item_type==="BR"){
+		    marker = L.marker([this.locations[i]['location_y'], this.locations[i]['location_x']], {icon: redIcon})
+		}
+		else {
+		    marker = L.marker([this.locations[i]['location_y'], this.locations[i]['location_x']], {icon: greyIcon})
+		}
                 marker.item_id = this.locations[i]['id']
                 marker.increment = this.locations[i]['increment']
                 marker.on('click', this.onMarkerClick);
                 marker.on('mouseover', this.onMarkerHover)
                 marker.on('mouseout', this.onMarkerOut)
-                this.markers.addLayer(marker)
+		markerList.push(marker)
+                //this.markers.addLayer(marker)
             }
-            this.map.addLayer(this.markers)
+	    this.markers.addLayers(markerList)
+            //this.map.addLayer(this.markers)
+	    this.map.addLayer(this.markers)
         }
     },
 }
