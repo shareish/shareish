545f9ac6e54ace38ae1039ca8b8a11363545db6d
rmaree <raphael.maree@ulg.ac.be>
2023-01-20 08:44:03 +0100

    update FAQ

--- frontend/src/components/HomeView.vue
+++ frontend/src/components/HomeView.vue
@@ -3,9 +3,10 @@
     <h1 class="title">{{$t('welcome')}} {{$t('on')}} SHAREISH ({{$t('shareish_title')}})</h1>
     <h2 class="subtitle">{{$t('about-us')}}</h2>
 
-<p>{{$t('shareish_description')}}.<br>
-Shareish {{$t('offers')}} <a @click="goto('/dashboard')" class="text-reset">{{$t('interactive_map')}}</a> {{$t('shareish_description2')}}.</p><br>
+<p>{{$t('shareish_description')}}.<br><br>
+<a @click="goto('/sign-up')" class="text-reset">{{$t('after_registration')}}</a> {{$t('andet')}} <a @click="goto('/log-in')" class="text-reset">{{$t('logged_in')}}</a>, Shareish {{$t('offers')}} <a @click="goto('/map')" class="text-reset">{{$t('interactive_map')}}</a> {{$t('shareish_description2')}}.</p><br>
 
+<!--<b-image :src="require('@/assets/shareish-screenshot.png')" :rounded="rounded"></b-image>//-->
 <!--
 <p>Existing Shareish instances:</p>
 <ul><li><a href="https://shareish.org/">Shareish demonstration server</a></li>
@@ -15,14 +16,23 @@ Shareish {{$t('offers')}} <a @click="goto('/dashboard')" class="text-reset">{{$t
 <br>
 <h2 class="subtitle"><b>{{$t('faq')}}</b></h2>
 
-<p><b></b><br>
-<i></i>
 
 <p><b>{{$t('faq_nothing')}} </b><br>
 <i>{{$t('faq_nothing_answer')}}. </i></p><br>
 
 <p><b>{{$t('faq_everything')}}</b><br>
 <i>{{$t('faq_everything_answer')}} </i></p><br>
+
+<p><b>{{$t('faq_colors')}}</b><br>
+<i>{{$t('faq_colors_answer')}} </i></p><br>
+
+
+<p><b>{{$t('faq_address')}}</b><br>
+<i>{{$t('faq_address_answer')}} </i></p><br>
+
+<p><b>{{$t('faq_device')}}</b><br>
+<i>{{$t('faq_device_answer')}} </i></p><br>
+
 
 <p><b>{{$t('faq_data_collection')}}</b><br>
   <i>{{$t('faq_data_collection_answer')}}.</i></p><br>
