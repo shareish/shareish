2924ffebc2b76d9a33eed7765c9f6746dd6afe2d
Izanami <ogulcan.sevinc@student.hepl.be>
2024-02-23 22:46:47 +0100

    db+images backup procedure

---
+++ backup/deploy.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2009-2017. Authors: see NOTICE file.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+
+mkdir -p /var/shareish/
+mkdir -p /var/shareish/mediafiles
+
+cp /tmp/script_backup_postgres.sh /var/shareish/script_backup.sh
+cp /tmp/script_backup_mediafiles.sh /var/shareish/mediafiles/script_backup_mediafiles.sh
+	
+
+chmod +x /var/shareish/script_backup.sh
+chmod +x /var/shareish/mediafiles/script_backup_mediafiles.sh
+
+
+
+
+#hostname:port:database:username:password
+echo "db:*:$POSTGRES_DB:$POSTGRES_USER:$POSTGRES_PASSWORD" > /root/.pgpass
+cat /root/.pgpass
+chmod 600 /root/.pgpass
+
+echo "Add the backup script to crontab"
+touch /tmp/crontab
+echo "#Setting env var" >> /tmp/crontab
+
+echo "DATABASE=postgres" >> /tmp/crontab
+echo "USER=postgres" >> /tmp/crontab
+
+
+echo "CONTAINER=db" >> /tmp/crontab
+echo "#End setting env var" >> /tmp/crontab
+
+echo "30 23 * * */var/shareish/script_backup.sh postgres" >> /tmp/crontab
+echo "30 23 * * */var/shareish/mediafiles/script_backup_mediafiles.sh" >> /tmp/crontab
+crontab /tmp/crontab
+rm /tmp/crontab
+
+service rsyslog restart
+service cron restart
+
+touch /var/shareish/backup_database.log
+touch /var/shareish/mediafiles/backup_mediafiles.log
+tail -f /var/shareish/backup_database.log
+tail -f var/shareish/mediafiles/backup_mediafiles.log
+
+
---
+++ backup/script_backup_postgres.sh
@@ -0,0 +1,63 @@
+#! /bin/bash
+
+#
+# Copyright (c) 2009-2017. Authors: see NOTICE file.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+Backuplog="/var/shareish/backup_database.log"
+BackupDatabase=$DATABASE
+BackupTime=`date '+%Y-%m-%d_%H-%M'`
+BackupOutDir="/backup/shareish_database/$BackupDatabase"
+BackupFile="$BackupOutDir/PRODbackup-$BackupDatabase-$BackupTime.sql"
+
+
+
+
+mkdir -p ${BackupOutDir}
+
+echo "Starting Backup of Shareish Prod DB at : ${BackupTime}"             > ${Backuplog}
+echo "================================================================"         >> ${Backuplog}
+echo ""                                                                         >> ${Backuplog}
+echo "Starting pg_dump to ${BackupFile} ..."                                    >> ${Backuplog}
+touch ${BackupFile}
+chmod a+rw ${BackupFile}
+echo "pg_dump -h db -U postgres -w -f ${BackupFile} $BackupDatabase"        >> ${Backuplog} 2>&1
+pg_dump -h db -U postgres -w -f ${BackupFile} $BackupDatabase               >> ${Backuplog} 2>&1      
+PGDumpRC=$?
+echo "  EXIT_CODE:${PGDumpRC}"                                                  >> ${Backuplog}
+echo ""                                                                         >> ${Backuplog}
+echo "Removing backup files older than 7 days ..."                              >> ${Backuplog}
+echo ""                                                                         >> ${Backuplog}
+find ${BackupOutDir} -name *.sql -ctime +7 -exec rm -f -v {} \;  |\
+ sed -e "s/^removed/  removed/g"                                                >> ${Backuplog} 2>&1
+echo ""                                                                         >> ${Backuplog}
+echo "Following backup files left in ${BackupOutDir} ..."                          >> ${Backuplog}
+echo ""                                                                         >> ${Backuplog}
+ls -1t ${BackupOutDir} | sed -e "s/^PROD/  PROD/g"                                 >> ${Backuplog}
+
+
+
+
+if [ $PGDumpRC -gt 0 ]
+then
+    echo "ERROR: Backup of ${BackupDatabase} failed. See ${Backuplog} for details."  >> ${Backuplog}
+
+else
+     echo "Backup of ${BackupDatabase} DB successful." >> ${Backuplog}
+fi
+
+
+   
