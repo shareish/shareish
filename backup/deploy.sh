703d609810ec159acdb4a1f566afa3a9870a6aad
Izanami <ogulcan.sevinc@student.hepl.be>
2024-02-27 11:26:27 +0100

    Add compose env file for docker-compose

--- backup/deploy.sh
+++ backup/deploy.sh
@@ -32,7 +32,7 @@ chmod +x /var/shareish/mediafiles/script_backup_mediafiles.sh
 
 
 #hostname:port:database:username:password
-echo "$POSTGRES_CONTAINER:5432:$POSTGRES_DB:$POSTGRES_USER:postgres" > /root/.pgpass
+echo "$POSTGRES_CONTAINER:$POSTGRES_PORT:$POSTGRES_DB:$POSTGRES_USER:$POSTGRES_PASSWORD" > /root/.pgpass
 cat /root/.pgpass
 chmod 600 /root/.pgpass
 
