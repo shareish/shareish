72e1822a48de443d50fd8676d88f371d0db2d7ea
Hoyoux Adrien <hoyouxadrien@gmail.com>
2024-03-05 12:18:22 +0100

    :tada: modify pwa configuration + add icons

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,19 +1,27 @@
 module.exports = {
-    // ...other vue-cli plugin options...
-    pwa: {
-      name: 'My App',
-      themeColor: '#4DBA87',
-      msTileColor: '#000000',
-      appleMobileWebAppCapable: 'yes',
-      appleMobileWebAppStatusBarStyle: 'black',
-  
-      // configure the workbox plugin
-      workboxPluginMode: 'InjectManifest',
-      workboxOptions: {
-        // swSrc is required in InjectManifest mode.
-        swSrc: 'dev/sw.js',
-        // ...other Workbox options...
-      }
+ 
+  pwa: {
+    
+    name: 'shareish',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    manifestOptions: {
+      name: 'Shareish',
+      short_name: 'Shareish',
+      start_url: '/',
+      display: 'standalone',
+      theme_color: '#4DBA87',
+      description : 'Shareish est un application solidaire !'
+    },
+    
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
+      // ...other Workbox options...
     }
   }
-  
+};
