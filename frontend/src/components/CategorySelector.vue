d76a33068fc2ebe31ac065b4f486da701f9aebc9
Hoyoux Adrien <hoyouxadrien@gmail.com>
2024-03-15 16:47:53 +0100

    :tada: added icons for addItem, itemFilter and editItem categories.

--- frontend/src/components/CategorySelector.vue
+++ frontend/src/components/CategorySelector.vue
@@ -1,24 +1,58 @@
 <template>
   <b-field>
-    <template #label>{{ label }}
+    <template #label>
+      {{ label }}
       <b-tooltip v-if="usesTooltip" :label="$t('help_item_category')" multilined position="is-right">
         <i class="icon far fa-question-circle"></i>
       </b-tooltip>
     </template>
-    <b-select :expanded="expanded" :value="value" @input="$emit('input', $event)">
-      <option v-if="number > 1 || !number" value=""></option>
-      <option v-for="{id, slug} in categories" :key="id" :value="id">
-        {{ $t(slug) }}
-      </option>
-    </b-select>
+    <b-dropdown
+      position="is-top-right"
+      class="dropdown-style"
+      v-model="selectedCategory"
+      aria-role="list"
+      scrollable
+      @input="$emit('input', $event)">
+      <template #trigger>
+        <b-button
+          icon-right="fas fa-angle-down"
+          :icon-left="selectedCategory ? categories[selectedCategory].icon : (value ? categories[value].icon : '')"
+          type="is-white"
+          class="truncated-text"
+        >
+      <span>
+        {{selectedCategory ? $t(categories[selectedCategory].slug) : (value ? $t(categories[value].slug) : $t('select_category'))}}
+      </span>
+      </b-button>
+        
+      </template>
+      <b-dropdown-item
+        v-for="(category, key) in categories"
+        :key="key"
+        :value="key"
+        aria-role="listitem"
+      >
+        <div class="media">
+          <b-icon class="media-left" :icon="category.icon"></b-icon>
+          <div class="media-content">
+            {{$t(category.slug)}}
+          </div>
+        </div>
+      </b-dropdown-item>
+    </b-dropdown>
   </b-field>
 </template>
 
 <script>
-import {categories} from "@/categories";
+import { categories } from "@/categories";
 
 export default {
   name: 'CategorySelector',
+  data() {
+    return {
+      selectedCategory: this.value || null,
+    }
+  },
   props: {
     number: Number,
     value: String,
@@ -37,8 +71,16 @@ export default {
       return this.$tc('category', 1) + category_number;
     },
     categories() {
-      return Object.values(categories);
+      return categories;
     }
-  }
+  },
 };
 </script>
+
+<style scoped>
+.truncated-text {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
