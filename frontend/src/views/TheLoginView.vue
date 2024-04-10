278436d70959eddfdcd6ebd4026b66b81aa6381b
Hoyoux Adrien <adrien.hoyoux@student.hepl.be>
2024-04-10 11:13:12 +0200

    change loginView appearance
    add buttons for the other connection methods
    
    added verification for password and authValue fields
    
    add a b-message if the account is not retrieved from the api

--- frontend/src/views/TheLoginView.vue
+++ frontend/src/views/TheLoginView.vue
@@ -1,32 +1,59 @@
 <template>
   <div id="page-log-in" class="columns">
-    <div class="column is-4 is-offset-4">
+    <div class="column is-6 is-offset-3">
       <h1 class="title">{{ $t('log-in') }}</h1>
-      <div class="field">
-        <label>{{ $t('email-or-username') }}</label>
-        <div class="control">
-          <b-input v-model="authValue" name="authValue" type="text" icon-pack="fas" icon="user" icon-right="close-circle" icon-right-clickable
-                @icon-right-click="clearAuthIconClick"/>
-        </div>
-      </div>
+      <b-message v-if="!responseOK" :title="$t('error_keys__BAD_LOGIN')" type="is-danger">
+        {{ $t('error_keys__ACCOUNT_DOESNT_EXIST') }}
+      </b-message>
+        <b-field :label="$t('email-or-username')" :message="errors.first('authValue')" :type="{'is-danger': errors.has('authValue')}">
+          <b-input v-model="authValue" name="authValue" icon-pack="fas" icon="user" icon-right="close-circle" icon-right-clickable
+                @icon-right-click="clearAuthIconClick" 
+                v-validate="'required'"
+                />
+        </b-field>
       <b-message v-if="showDisabledAccountLink" title="Account disabled" type="is-warning">
         {{ $t('help_login-disabled-account') }} <router-link :to="{name: 'recoverAccount'}">{{ $t('click-here') }}</router-link>.
       </b-message>
       <b-message v-if="showScheduledDeletionAccountLink" title="Account scheduled for deletion" type="is-danger">
         <span v-html="$t('help_login-scheduled-deletion-account', {x: daysBeforeDeletion})" /> <router-link :to="{name: 'recoverAccount'}">{{ $t('click-here') }}</router-link>.
-      </b-message>
-      <div class="field">
-        <label>{{ $t('password') }}</label>
-        <div class="control">
-          <b-input v-model="password" name="password" password-reveal type="password" icon-pack="fas" icon="lock"/>
-        </div>
-      </div>
-      <div class="field">
-        <div class="control">
-          <b-button type="is-primary" :loading="waitingFormResponse" @click="submitForm">{{ $t('log-in') }}</b-button>
-        </div>
-      </div>
+      </b-message> 
+      <b-field :label="$t('password')" :message="errors.first('password')" :type="{'is-danger': errors.has('password')}">
+        <b-input v-model="password" name="password" password-reveal type="password" icon-pack="fas" icon="lock" v-validate="'required'"/>
+      </b-field>
       <router-link :to="{name: 'resetPassword'}">{{ $t('password-forgotten-?') }}</router-link>
+      <b-button class="mt-4 ml-0" type="is-primary" :loading="waitingFormResponse" @click="submitForm" expanded>{{ $t('log-in') }}</b-button>
+      <div class="has-text-centered"> <!-- Pour centrer le contenu -->
+        <p class="mt-5">{{ $t('OR') }}</p>
+        <hr class="mt-2"> 
+      </div>
+      <b-button type="is-warning is-light"
+        label="GOOGLE"
+        icon-pack="fab"
+        icon-left="fab fa-google"
+        expanded
+        class="ml-0">
+      </b-button>
+      <b-button type="is-warning is-light"
+        label="FACEBOOK"
+        icon-pack="fab"
+        icon-left="fab fa-facebook"
+        expanded
+        class="ml-0 mt-2">
+      </b-button>
+      <b-button type="is-warning is-light"
+        label="TWITTER"
+        icon-pack="fab"
+        icon-left="fab fa-twitter"
+        expanded
+        class="ml-0 mt-2">
+      </b-button>
+      <b-button type="is-warning is-light"
+        label="MASTODON"
+        icon-pack="fab"
+        icon-left="fab fa-mastodon"
+        expanded
+        class="ml-0 mt-2">
+      </b-button>
     </div>
   </div>
 </template>
@@ -38,6 +65,9 @@ import ErrorHandler from "@/mixins/ErrorHandler";
 export default {
   name: "TheLoginView",
   mixins: [ErrorHandler],
+  $_veeValidate: {
+    validator: 'new'
+  },
   data() {
     return {
       authValue: '',
@@ -45,7 +75,8 @@ export default {
       waitingFormResponse: false,
       showDisabledAccountLink: false,
       showScheduledDeletionAccountLink: false,
-      daysBeforeDeletion: 0
+      daysBeforeDeletion: 0,
+      responseOK: true,
     }
   },
   created() {
@@ -58,37 +89,42 @@ export default {
     async submitForm() {
       this.waitingFormResponse = true;
 
-      const formData = {
-        authValue: this.authValue,
-        password: this.password
-      }
+      let result = await this.$validator.validateAll();
 
-      try {
-        const auth = (await axios.post("/api/v1/auth/login/", formData)).data;
-
-        axios.defaults.headers.common["Authorization"] = "Token " + auth['token'];
-        this.$store.commit('setToken', auth['token']);
-        localStorage.setItem("token", auth['token']);
-        this.$store.commit('setUserID', auth['id']);
-        localStorage.setItem("user_id", auth['id']);
-        
-        await this.$router.push('/map');
-      }
-      catch (error) {
-        if (this.isKeyedError(error)) {
-          const key = this.getErrorKey(error);
-          console.log(key);
-          if (key === 'DISABLED_ACCOUNT') {
-            this.showDisabledAccountLink = true;
-          } else if (key === 'SCHEDULED_DELETION_ACCOUNT') {
-            this.showScheduledDeletionAccountLink = true;
-            this.daysBeforeDeletion = Number(error.response.data.days_left);
-          }
+      if(result){
+        const formData = {
+          authValue: this.authValue,
+          password: this.password
         }
-        this.snackbarError(error, {'showErrorCode': false});
+
+        try {
+          const auth = (await axios.post("/api/v1/auth/login/", formData)).data;
+
+          axios.defaults.headers.common["Authorization"] = "Token " + auth['token'];
+          this.$store.commit('setToken', auth['token']);
+          localStorage.setItem("token", auth['token']);
+          this.$store.commit('setUserID', auth['id']);
+          localStorage.setItem("user_id", auth['id']);
+          
+          await this.$router.push('/map');
+        }
+        catch (error) {
+          if (this.isKeyedError(error)) {
+            const key = this.getErrorKey(error);
+            console.log(key);
+            if (key === 'DISABLED_ACCOUNT') {
+              this.showDisabledAccountLink = true;
+            } else if (key === 'SCHEDULED_DELETION_ACCOUNT') {
+              this.showScheduledDeletionAccountLink = true;
+              this.daysBeforeDeletion = Number(error.response.data.days_left);
+            }
+          }
+          this.responseOK = false;
+        }
+
       }
 
-      this.waitingFormResponse = false;
+      this.waitingFormResponse = false;      
     }
   }
 }
