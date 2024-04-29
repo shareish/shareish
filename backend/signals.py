224ed725bb1f157e263f20a52e47fcea15e017a9
Izanami <ogulcan.sevinc@student.hepl.be>
2024-04-29 11:16:45 +0200

    fix: merge conflict

---
+++ backend/signals.py
@@ -0,0 +1,17 @@
+from allauth.account.signals import user_signed_up
+from django.dispatch import receiver
+from allauth.socialaccount.models import SocialAccount
+from mymap.models import User  # Importez votre modèle utilisateur personnalisé
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.db import models
+
+@receiver(post_save, sender=SocialAccount)
+def activate_user(sender, instance, created, **kwargs):
+    if created:
+        social_account_user_id = instance.user_id
+        print("ID du SocialAccount créé :", social_account_user_id)
+        user = User.objects.get(id=social_account_user_id)
+        user.is_active = True
+        user.save()
+        print("is_active mis à True.")
--- frontend/src/views/TheAddItemView.vue
+++ frontend/src/views/TheAddItemView.vue
@@ -117,7 +117,7 @@
                     <i class="icon far fa-question-circle"></i>
                   </b-tooltip>
                 </template>
-                <b-input v-model="description" expanded type="textarea" name="description" v-validate="'required'" />
+                <b-input ref="load" v-model="description" expanded type="textarea" name="description" v-validate="'required'"/>
               </b-field>
             </div>
           </div>
@@ -624,14 +624,14 @@ export default {
     },
     async processImage(file) {
       this.loading = true;
-
+      const loadingComponent = this.openLoading();
       const reader = new FileReader();
       reader.addEventListener('load', () => {
         this.images.push({"filename": file.name, 'predictions': [], 'preview': reader.result, 'probability': 0});
         this.fetchPredictions(file, this.images.length - 1);
       });
       reader.readAsDataURL(file);
-
+      loadingComponent.close();
       this.loading = false;
     },
     async fetchPredictions(file, position) {
@@ -805,6 +805,13 @@ export default {
         if (this.enddate < this.startdate)
           this.startdate = this.enddate;
       }
+    },
+    openLoading(){
+      const loadingComponent = this.$buefy.loading.open({
+        container : this.$refs.load.$el,        
+      })
+
+      return loadingComponent;
     },
     windowWidthChanged() {
       let imagesPreviewColumnSizeClass = "is-one-third";
