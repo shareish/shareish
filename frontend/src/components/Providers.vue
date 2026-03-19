e041b90f2bffd299ba40aff3fadef3984667073b
nicolas <nicolas@DESKTOP-32E8Q3I.home>
2026-03-19 17:49:34 +0100

    add icon provider with fontawesome

--- frontend/src/components/Providers.vue
+++ frontend/src/components/Providers.vue
@@ -11,33 +11,39 @@
                 </b-button>
             </b-tooltip>
             
-            <b-tooltip label="Google" type="is-dark" position="is-bottom">
-                <b-button @click="oidcGoogle">
-                    <b-icon pack="fab" icon="google" size="is-medium" type="is-primary"></b-icon>
+            <b-tooltip label="Openstreetmap" type="is-dark" position="is-bottom">
+                <b-button @click="oidcOSM">
+                    <b-icon pack="fa" icon="map" size="is-medium" type="is-primary"></b-icon>
                 </b-button>
             </b-tooltip>
                               
-            <b-tooltip label="Google" type="is-dark" position="is-bottom">
-                <b-button @click="oidcGoogle">
-                    <b-icon pack="fab" icon="google" size="is-medium" type="is-primary"></b-icon>
+            <b-tooltip label="Wikipedia" type="is-dark" position="is-bottom">
+                <b-button @click="oidcWikipedia">
+                    <b-icon pack="fab" icon="wikipedia-w" size="is-medium" type="is-primary"></b-icon>
                 </b-button>
             </b-tooltip>
 
-            <b-tooltip label="Google" type="is-dark" position="is-bottom">
-                <b-button @click="oidcGoogle">
-                    <b-icon pack="fab" icon="google" size="is-medium" type="is-primary"></b-icon>
+            <b-tooltip label="Facebook" type="is-dark" position="is-bottom">
+                <b-button @click="oidcFacebook">
+                    <b-icon pack="fab" icon="facebook" size="is-medium" type="is-primary"></b-icon>
                 </b-button>
             </b-tooltip>
 
-            <b-tooltip label="Google" type="is-dark" position="is-bottom">
-                <b-button @click="oidcGoogle">
-                    <b-icon pack="fab" icon="google" size="is-medium" type="is-primary"></b-icon>
+            <b-tooltip label="Microsoft" type="is-dark" position="is-bottom">
+                <b-button @click="oidcMicrosoft">
+                    <b-icon pack="fab" icon="microsoft" size="is-medium" type="is-primary"></b-icon>
                 </b-button>
             </b-tooltip>
 
-            <b-tooltip label="Google" type="is-dark" position="is-bottom">
-                <b-button @click="oidcGoogle">
-                    <b-icon pack="fab" icon="google" size="is-medium" type="is-primary"></b-icon>
+            <b-tooltip label="Apple" type="is-dark" position="is-bottom">
+                <b-button @click="oidcApple">
+                    <b-icon pack="fab" icon="apple" size="is-medium" type="is-primary"></b-icon>
+                </b-button>
+            </b-tooltip>
+
+            <b-tooltip label="Github" type="is-dark" position="is-bottom">
+                <b-button @click="oidcGithub">
+                    <b-icon pack="fab" icon="github" size="is-medium" type="is-primary"></b-icon>
                 </b-button>
             </b-tooltip>
         </div>
@@ -49,7 +55,25 @@ export default({
     methods:{
         async oidcGoogle(){
             window.location.href = "http://localhost:8000/accounts/google/login/";
-        }
+        },
+        async oidcOSM(){
+            window.location.href = "http://localhost:8000/accounts/openstreetmap/login/";
+        },
+        async oidcWikipedia(){
+            window.location.href = "http://localhost:8000/accounts/mediawiki/login/";
+        },
+        async oidcFacebook(){
+            window.location.href = "http://localhost:8000/accounts/facebook/login/";
+        },
+        async oidcMicrosoft(){
+            window.location.href = "http://localhost:8000/accounts/microsoft/login/";
+        },
+        async oidcApple(){
+            window.location.href = "http://localhost:8000/accounts/apple/login/";
+        },
+        async oidcGithub(){
+            window.location.href = "http://localhost:8000/accounts/github/login/";
+        },
     },
 })
 </script>
